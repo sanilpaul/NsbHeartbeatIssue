c19582f825cb3753d63899ba43f801fdf0c58bd9
Sanil Paul <sanilpaul@gmail.com>
2021-03-24 10:37:51 -0400

    with configure heartbeats

---
+++ generic-multi-hosting_core_7_netcoreapp3_1/Sample.Sender/ConfigureHeartBeats.cs
@@ -0,0 +1,18 @@
+﻿namespace Sample.Sender
+{
+    using System;
+    using NServiceBus;
+
+    class ConfigureHeartBeats : INeedInitialization
+    {
+        public void Customize(EndpointConfiguration configuration)
+        {
+            var serviceControlQueue = "Particular.Heartbeats.Servicecontrol";
+
+            var transport = configuration.UseTransport<SqlServerTransport>();
+            transport.UseSchemaForQueue(serviceControlQueue, "dbo");
+            var heartBeatsInterval = TimeSpan.FromSeconds(5);
+            configuration.SendHeartbeatTo(serviceControlQueue, frequency: heartBeatsInterval);
+        }
+    }
+}
--- generic-multi-hosting_core_7_netcoreapp3_1/Sample.Sender/Program.cs
+++ generic-multi-hosting_core_7_netcoreapp3_1/Sample.Sender/Program.cs
@@ -62,17 +62,4 @@ namespace Sample.Sender
             return Endpoint.Start(endpointConfiguration);
         }
     }
-
-    class ConfigureHeartBeats : INeedInitialization
-    {
-        public void Customize(EndpointConfiguration configuration)
-        {
-            var serviceControlQueue = "Particular.Heartbeats.Servicecontrol";
-
-            var transport = configuration.UseTransport<SqlServerTransport>();
-            transport.UseSchemaForQueue(serviceControlQueue, "dbo");
-            var heartBeatsInterval = TimeSpan.FromSeconds(5);
-            configuration.SendHeartbeatTo(serviceControlQueue, frequency: heartBeatsInterval);
-        }
-    }
 }
